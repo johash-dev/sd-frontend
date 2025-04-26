c28bd2f66467fa392d7ebdb4f6c75b610414678e
johash-dev <johash.dev@gmail.com>
2025-04-26 22:18:57 +0530

    chore: basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import './App.css'
+import './App.css';
+import { Layout } from './components/layout/Layout';
+import { Button } from './components/ui/button';
 
 function App() {
-  
-
   return (
     <>
-      
+      <Layout>
+        <Button>Test</Button>
+      </Layout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactNode } from 'react';
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const Layout: FC<LayoutProps> = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export { Layout };
